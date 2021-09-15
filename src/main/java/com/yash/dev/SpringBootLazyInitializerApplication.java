04558fa07135188f3ca4ea14be8645940abb33f6
Yashwanth Anand S <yashwanthanands@outlook.com>
2021-09-15 20:39:40 +0530

    Commit with Initial Project Code

---
+++ src/main/java/com/yash/dev/SpringBootLazyInitializerApplication.java
@@ -0,0 +1,13 @@
+package com.yash.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLazyInitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLazyInitializerApplication.class, args);
+	}
+
+}
